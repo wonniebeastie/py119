73b766cd9321604c42c8da02d39741a40f99ca3f
Wonnie <wonnie.tech@gmail.com>
2025-08-05 13:15:40 -0700

    Add another solution to problem 4

--- py119_interview_practice_problems/4.py
+++ py119_interview_practice_problems/4.py
@@ -35,6 +35,7 @@ Breakdown:
             - UPDATE closest_pair to current pair (numbers[i] & numbers[j])
 - RETURN closest_pair
 """
+# APPROACH 1
 def closest_numbers(numbers):
     smallest_diff = float('inf')
     closest_pair = ()
@@ -48,6 +49,24 @@ def closest_numbers(numbers):
 
     return closest_pair
 
-print(closest_numbers([5, 25, 15, 11, 20]) == (15, 11))
-print(closest_numbers([19, 25, 32, 4, 27, 16]) == (25, 27))
-print(closest_numbers([12, 22, 7, 17]) == (12, 7))
+print(closest_numbers([5, 25, 15, 11, 20]) == (15, 11)) # True
+print(closest_numbers([19, 25, 32, 4, 27, 16]) == (25, 27)) # True
+print(closest_numbers([12, 22, 7, 17]) == (12, 7)) # True
+
+# APPROACH 2
+# A less efficient alternative solution.
+def get_difference(pair):
+    return max(pair) - min(pair)
+
+def closest_numbers2(numbers):
+    pairs = []
+    for idx1 in range(len(numbers) - 1):
+        for idx2 in range(idx1 + 1, len(numbers)):
+            pairs.append((numbers[idx1], numbers[idx2]))
+
+    pairs.sort(key=get_difference)
+    return pairs[0]
+
+print(closest_numbers([5, 25, 15, 11, 20]) == (15, 11)) # True
+print(closest_numbers([19, 25, 32, 4, 27, 16]) == (25, 27)) # True
+print(closest_numbers([12, 22, 7, 17]) == (12, 7)) # True
